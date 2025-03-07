a453749a681a480e24bb2f3ffc69cb5a2a2e2b64
Fatiha Tabassum <tabassum.fatiha05@gmail.com>
2025-03-07 12:16:01 +0000

    vegenere cipher code template (from copilot then modified); compiles but doesn't run compliainig about some power issues

--- main.s
+++ main.s
@@ -1,58 +1,157 @@
 #include <xc.inc>
 
-extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex ; external LCD subroutines
+extrn LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_I, LCD_delay_ms, LCD_Send_Byte_D
 
-	
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+psect udata_acs
+counter_pt: ds 1
+counter_ec: ds 1
+key_index: ds 1
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a  ; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
-    
-psect	code, abs	
-rst: 	org 0x0
- 	goto	setup
+psect udata_bank4
+CiphertextArray: ds 0x80
 
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	LCD_Setup	; setup LCD
-	goto	start
-	
-	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
+psect data
+KeyTable:
+    db 'K','E','Y'
+KeyLength EQU 3
+align 2
 
-	movlw	myTable_l-1	; output message to LCD
-				; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
-	
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
+psect code
+PlaintextArray:
+    db 'P','l','a','i','n','t','e','x','t'
+TableLength EQU 9
 
-	end	rst
+psect code, abs
+rst: org 0x0
+    goto setup
+
+setup:
+    bcf CFGS
+    bsf EEPGD
+    call LCD_Setup
+    movlw 0x00
+    movwf TRISH, A
+    movlw 0x00
+    movwf PORTH, A
+    goto start
+
+start:
+    call print_plaintext
+    movlw 0xC0
+    call LCD_Send_Byte_I
+
+    movlw 0xFF
+    call LCD_delay_ms
+
+    call vigenere_encrypt
+    call print_ciphertext
+
+    movlw 0xFF
+    call LCD_delay_ms
+    movlw 0xFF
+    call LCD_delay_ms
+    movlw 0xFF
+    call LCD_delay_ms
+    movlw 0xFF
+    call LCD_delay_ms
+
+    goto ending
+
+vigenere_encrypt:
+    lfsr 0, CiphertextArray
+    lfsr 2, KeyTable
+
+    movlw TableLength
+    movwf counter_ec, A
+
+    clrf key_index, A
+
+    goto encrypt_loop
+
+encrypt_loop:
+    movf counter_ec, W, A
+    bz encrypt_done
+
+    movlw HIGH(PlaintextArray)
+    movwf TBLPTRH
+    movlw LOW(PlaintextArray)
+    addwf counter_ec, W
+    movwf TBLPTRL
+    tblrd*+
+
+    movf TABLAT, W
+    sublw 'A'
+    movwf INDF1, A
+
+    movf INDF2, W, A
+    sublw 'A'
+    addwf INDF1, W, A
+    addlw 'A'
+    movwf POSTINC0, A
+
+    incf key_index, A
+    movf key_index, W, A
+    sublw KeyLength
+    btfsc STATUS, 2
+    clrf key_index, A
+
+    lfsr 2, KeyTable
+    addwf key_index, W, A
+    movwf FSR2L, A
+
+    decfsz counter_ec, A
+    bra encrypt_loop
+
+encrypt_done:
+    return
+
+print_ciphertext:
+    lfsr 0, CiphertextArray
+
+    movlw TableLength
+    movwf counter_pt, A
+    goto print_loop
+
+print_plaintext:
+    movlw TableLength
+    movwf counter_pt, A
+    goto print_plaintext_loop
+
+print_plaintext_loop:
+    movf counter_pt, W, A
+    bz print_plaintext_done
+
+    movlw HIGH(PlaintextArray)
+    movwf TBLPTRH
+    movlw LOW(PlaintextArray)
+    addwf counter_pt, W
+    movwf TBLPTRL
+    tblrd*+
+
+    movf TABLAT, W
+    call LCD_Send_Byte_D
+
+    decfsz counter_pt, A
+    bra print_plaintext_loop
+
+print_plaintext_done:
+    return
+
+print_loop:
+    movf counter_pt, W, A
+    bz print_done
+
+    movf POSTINC0, W, A
+    call LCD_Send_Byte_D
+
+    decfsz counter_pt, A
+    bra print_loop
+    goto print_done
+
+print_done:
+    return
+
+ending:
+    nop
+
+    end rst
