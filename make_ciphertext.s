0e634b28f845229a36fa3122eb4caf60aad8ee89
ft222 <ft222@skbl-418-009.ph.ic.ac.uk>
2025-03-13 10:30:19 +0000

    Vigenere cipher working + alphabet wrapping working, hard coded keyword repeat

--- make_ciphertext.s
+++ make_ciphertext.s
@@ -1,5 +1,5 @@
 #include <xc.inc>
-
+; Vigenere cipher
 extrn CiphertextArray, PlaintextArray, TableLength, counter_ec, KeyArray
 global modify_table
     
@@ -16,7 +16,6 @@ modify_table:
     ; Initialize FSR0 to point to CiphertextArray
     lfsr    2, CiphertextArray
 
-    
     movlw   TableLength          ; Load the number of characters to process
     movwf   counter_ec, A        ; Store in counter_ec
     
@@ -25,14 +24,24 @@ modify_table:
 modify_loop:
     movf    counter_ec, W, A     ; Check if counter is zero
     bz      modify_done          ; If zero, we are done
+
+    movlw   0x60 ; making 'a' shift = 1
+    subwf   POSTINC0, W
+
+    addwf   POSTINC1, W    ; Add the key
+
+    cpfslt  'z'                 ; Compare WREG with 'z'
     
-    movf    POSTINC0, W	    ; Read the plaintext character
-    addwf   POSTINC1, W    ; Add the key to PlainText
-    ;; alphabet warpping code need to implemented
-    movwf   POSTINC2, A             ; Write encrypted character to CiphertextArray
+    ;btfss   STATUS, 2, A            ; If greater, it's out of range
+    bra     wrap_done            ; If no wrap, skip
     
+    ; if greater then subtract 26 = 0x1A
+    ; Subtract 0x1A to wrap around
+    sublw   0x1A                 ; Subtract 'z' - 'a' (26)
+    
+wrap_done:    
+    movwf   POSTINC2, A         ; Write encrypted character to CiphertextArray
     decfsz  counter_ec, A        ; Decrement counter and check if done
-    
     bra     modify_loop          ; Loop again if not finished
 
 modify_done:
